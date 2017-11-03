274ba02346432249b3703162b2e3edb3cff6d736
Matheus Soares <m.soares.ms@outlook.com>
2017-11-03 16:31:27 -0400

    vector<Vertex> na classe Grafo

---
+++ Grafo.h
@@ -0,0 +1,29 @@
+#ifndef BFS_GRAFO_H
+#define BFS_GRAFO_H
+
+#include <vector>
+using namespace std;
+
+typedef int Vertex;
+class Grafo {
+private:
+    vector<Vertex> * adj;
+    int ordem, tamanho;
+    void destroi();
+
+public:
+    Grafo(int pN){ inicializar(pN); }
+
+    void inicializar(int);
+    void inserirAresta(Vertex, Vertex);
+    void mostrar();
+
+    int getOrdem() { return this->ordem; }
+    void setOrdem(int pOrdem) { this->ordem = pOrdem; }
+    int getTamanho() { return this->tamanho; }
+    void setTamanho(int pTamanho) { this->tamanho = pTamanho; }
+    vector<Vertex> * getAdj() { return this->adj; }
+};
+
+
+#endif //BFS_GRAFO_H
